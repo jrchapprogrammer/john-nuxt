563dc2c57d3f4a9b2b6639c8cc7b2c53d8762643
John Chappelle <jrchap82@gmail.com>
2018-05-04 14:20:56 -0400

    Added content to contact and projects

---
+++ pages/contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contact Me</h1>
+      <p>
+        You can email me at: <a href="mailto:jrchap82@gmail.com" target="_blank">jrchap82@gmail.com</a>.
+      </p>
+      <p>
+        Connect with me on <a href="https://www.linkedin.com/in/john-chappelle-4b2a0719" target="_blank">LinkedIn</a>.
+      </p>
+      <p>
+        Find me on <a href="https://www.facebook.com/jchappelleiii" target="_blank">Facebook</a>.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Contact"
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/projects.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">My Projects</h1>
+      <p>
+        Some of my projects can be found on <a href="https://github.com/jrchapprogrammer" target="_blank">GitHub</a>.
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Projects"
+};
+</script>
+
+<style scoped>
+
+</style>
