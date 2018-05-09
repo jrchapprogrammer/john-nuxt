7570cbe8aba0a01bf9b5e22d5ea121772849b0d1
John Chappelle <jrchap82@gmail.com>
2018-05-09 19:29:24 -0400

    fixed typo

--- pages/about.vue
+++ pages/about.vue
@@ -4,7 +4,7 @@
        <img src="../assets/537383_4851088768154_1771676081_n-1.jpg" alt="John Chappelle image">
        <p>I am a Philadelphia-based, freelance web developer. I make websites, web applications, and am dabbling in mobile apps. I mainly work with front-end technologies, but I learn new things everyday.</p>
        <p>I love taking things apart and putting them back together. My dad always taught me to know how things work, even if you don't have to physically make them work.</p>
-       <p>My life and work experience span different sectors and industries, which has helped me to lk for solutions from many different viewpoints.</p> 
+       <p>My life and work experience span different sectors and industries, which has helped me to look for solutions from many different viewpoints.</p> 
        <p>For prospective employers, you can find my technical resume <a href="https://jrchapprogrammer.github.io">here</a>.</p> 
     </div>
 </template>
