bff243f8fddc3966bf28271a492cdea32185ed81
John Chappelle <jrchap82@gmail.com>
2018-05-10 13:11:01 -0400

    hotfix: fixed resume page link to not bounce from site

--- pages/about.vue
+++ pages/about.vue
@@ -5,7 +5,7 @@
        <p>I am a Philadelphia-based, freelance web developer. I make websites, web applications, and am dabbling in mobile apps. I mainly work with front-end technologies, but I learn new things everyday.</p>
        <p>I love taking things apart and putting them back together. My dad always taught me to know how things work, even if you don't have to physically make them work.</p>
        <p>My life and work experience span different sectors and industries, which has helped me to look for solutions from many different viewpoints.</p> 
-       <p>For prospective employers, you can find my technical resume <a href="https://jrchapprogrammer.github.io">here</a>.</p> 
+       <p>For prospective employers, you can find my technical resume <a href="https://jrchapprogrammer.github.io" target="_blank">here</a>.</p> 
     </div>
 </template>
 
