488e591afddbf21b8b5c6957c823d9ec090b038a
John Chappelle <jrchap82@gmail.com>
2018-05-14 18:07:31 -0400

    added axios, setup asyncData for pulling repos from Github API

---
+++ components/GitRepo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <!-- iterates over Github repos and outputs select repo data -->
+        <ul v-if="projects && projects.length">
+            <li v-for="project of projects" :key="project">
+                <a :href="url" target="_blank">
+                    <h2>{{ repoName }}</h2>
+                </a>
+                <p><strong>{{ owner }}</strong></p>
+                <p>{{ made }}</p>
+                <p>{{ description }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "GitRepo",
+  props: ["url", "repoName", "owner", "made", "description"]
+};
+</script>
+
+<style scoped>
+li {
+  list-style-type: none;
+}
+a {
+  text-decoration: none;
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -2,6 +2,7 @@
   <section class="section is-medium">
     <div class="container has-text-centered">
       <h1 class="title">My Projects</h1>
+      <GitRepo :url='url' :repoName='repoName' :owner='owner' :made='made' :description='description'/>
       <p>
         Some of my projects can be found on <a href="https://github.com/jrchapprogrammer" target="_blank">GitHub</a>.
       </p>
@@ -10,8 +11,33 @@
 </template>
 
 <script>
+import axios from "axios";
+import GitRepo from "../components/GitRepo";
 export default {
-  name: "Projects"
+  components: GitRepo,
+  name: "Projects",
+  data() {
+    return {
+      data: []
+    };
+  },
+  async asyncData({ params }) {
+    let { data } = await axios.get(
+      `https://api.github.com/users/jrchapprogrammer/repos?type=owner${
+        params.id
+      }`
+    );
+    return { project: data };
+  },
+  head() {
+    return {
+      url: this.project.url,
+      repoName: this.project.name,
+      owner: this.project.owner,
+      made: this.project.deployments_url.created_url,
+      description: this.project.description
+    };
+  }
 };
 </script>
 
